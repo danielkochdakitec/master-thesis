02f48ca4d5da3df0b935cc6b9e8b85053dbaa273
Daniel Koch <daniel.koch@dakitec.de>
2022-01-24 14:51:14 +0100

    More graphics

---
+++ latex/tex/src/vergleich-kosten/functions_transfer.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('science')
+
+x = np.linspace(0, 60, 100)
+
+plt.plot(x, 0.09 * x, '-', label='AWS Lambda', color='#FF9900', alpha=0.8, lw=1)
+plt.plot(x, ((0.12 * x) - (5 * 0.12)).clip(min=0.0), '-', label='Google Cloud Functions', color='#4285F4', alpha=0.8, lw=1)
+
+plt.xlabel('Übertragene Daten (GB)', color='#1C2833')
+plt.ylabel('Kosten (US-Dollar)', color='#1C2833')
+
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+plt.grid()
+plt.savefig('../../bilder/7_1_vergleich-cloud-kosten/functions-transfer.pdf')
+plt.show()
