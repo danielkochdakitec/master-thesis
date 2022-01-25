a0e646385d98c5d0117090434460b424d69c96f7
Daniel Koch <daniel.koch@dakitec.de>
2022-01-25 20:20:49 +0100

    stuff

--- latex/tex/src/vergleich-kosten/functions_compute.py
+++ latex/tex/src/vergleich-kosten/functions_compute.py
@@ -6,7 +6,7 @@ plt.style.use('science')
 x = np.linspace(0, 100000, 1000)
 
 plt.plot(x, 60 * ((0.0000166667 * x) - (1600000/60 * 0.0000166667)).clip(min=0.0), '-', label='AWS Lambda', color='#FF9900', alpha=0.8, lw=1)
-plt.plot(x, 60 * ((0.0000035 * x) - (1600000/60 * 0.0000035)).clip(min=0.0), '-', label='Google Cloud Functions', color='#4285F4', alpha=0.8, lw=1)
+plt.plot(x, 60 * ((0.0000175 * x) - (1600000/60 * 0.0000175)).clip(min=0.0), '-', label='Google Cloud Functions', color='#4285F4', alpha=0.8, lw=1)
 
 plt.xlabel('Ausführungszeit (min)', color='#1C2833')
 plt.ylabel('Kosten (US-Dollar)', color='#1C2833')
